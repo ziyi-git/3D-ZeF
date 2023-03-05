11385028d973ac72c10f3ea6639aa1657fe2b784
Liu Ziyi <lc_lc2000@hotmail.com>
2023-03-05 19:59:52 +0800

    add video_utils folder

---
+++ video_utils/__init__.py
@@ -0,0 +1,9 @@
+from .video_preprocess import create_video, merge_videos, cut_video, resize_video
+# from .multithread import Worker
+
+all = [
+    'create_video',
+    'merge_videos',
+    'cut_video',
+    'resize_video'
+]
